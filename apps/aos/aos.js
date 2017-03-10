344c23b092171e09f66ad821a542b7a6500f1629
npavangouda <npavangouda@gmail.com>
2017-03-10 09:27:57 +0530

    added agent api call

--- apps/aos/aos.js
+++ apps/aos/aos.js
@@ -1,16 +1,26 @@
 var SpeechResponse = require('./../../shared/data-models/speechResponse.js');
 var Speech = require('./../../shared/data-models/speech');
 var Utilities = require('./../../shared/utilities/utilities.js');
+var agents = require('./../../shared/data-models/agent.js');
+var Session = require('./../../shared/data-models/session.js');
+
 var q = require('q');
 var request = require('request');
 
 var AOS = function () { };
+
+//#region CONSTANTS
+var URL_COMMON = "https://purchase-stest.allstate.com/onlinesalesapp-common/";
+var URL_RENTERS_SESSIONID = URL_COMMON + "api/transaction/RENTERS/sessionid";
+var URL_GETAGENTS = URL_COMMON + "api/common/agents";
+var URL_GETSTATE = URL_COMMON + "api/location/{0}/state";
 
 var AGENTFINDRESP = [
     "Sure. what's your zip code?",
     "I can help you with that. What's your zip?",
     "Please provide the zip?",
 ];
+//#endregion
 
 AOS.prototype.handleAgentFindRequest = function (sessionAttrs) {
     var deferred = q.defer();
@@ -56,10 +66,89 @@ AOS.prototype.handleAgentFindByZipIntent = function (sessionAttrs) {
 function getFinalAgentFindResponse(zip) {
     var deferred = q.defer();
     var finalSpeechOutput = new Speech();
-    finalSpeechOutput.text = "your agents are 13413.";
+    var sessionInfo = new Session();
+    sessionInfo.zip = zip;
 
-    deferred.resolve(finalSpeechOutput);
+    startAOSSession()
+        .then(function (id) {
+            sessionInfo.sessionId = id;
+            return getStateFromZip(sessionInfo.sessionId, zip);
+        }).then(function (state) {
+            sessionInfo.state = state;
+            return getAgents(sessionInfo);
+        }).then(function (agentsResp) {
+            if (agentsResp && agentsResp.agentAvailable) {
+                var firstAgentName = agentsResp.agents[0].name;
+                finalSpeechOutput.text = "nearest Allstate agent to you is, " + firstAgentName + 
+                                        ". You can call the agent at " + agentsResp.agents[0].phoneNumber + 
+                                        ". Or, would like me to email you the agent details?";
+            } else {
+                finalSpeechOutput.text = "sorry! no agents are available at zip " + sessionInfo.zip;
+            }
+            deferred.resolve(finalSpeechOutput);
+        }).catch(function (error) {
+            finalSpeechOutput.text = "something went wrong with agent service. Please try again later.";
+            deferred.resolve(finalSpeechOutput);
+        })
+
     return deferred.promise;
 };
+
+
+function getAgents(sessionInfo) {
+    var deferred = q.defer();
+    request(
+        {
+            method: 'POST',
+            url: URL_GETAGENTS,
+            "content-type": "application/json",
+            headers: {
+                "X-TID": sessionInfo.sessionId,
+                "X-ST": sessionInfo.state
+            },
+            json: true,
+            body: { "zipCode": sessionInfo.zip }
+        },
+        function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                errormsg = "Error from server session";
+                deferred.reject(errormsg);
+            } else {
+                deferred.resolve(response.body);
+            }
+        });
+
+    return deferred.promise;
+}
+
+function startAOSSession(zip) {
+    var deferred = q.defer();
+    request({ method: 'GET', uri: URL_RENTERS_SESSIONID }, function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+            errormsg = "Error from server session";
+            deferred.reject(errormsg);
+        } else {
+            var sessionId = response.headers['x-tid'];
+            deferred.resolve(sessionId);
+        }
+    });
+    return deferred.promise;
+}
+
+function getStateFromZip(sessionId, zip) {
+    var deferred = q.defer();
+    var urlGetStateFromZip = URL_GETSTATE.replace("{0}", zip);
+    request({ method: 'GET', uri: urlGetStateFromZip, headers: { "X-TID": sessionId } },
+        function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                errormsg = "Error from server session";
+                deferred.reject(errormsg);
+            } else {
+                var responseJson = JSON.parse(response.body);
+                deferred.resolve(responseJson.stateCode);
+            }
+        });
+    return deferred.promise;
+}
 
 module.exports = new AOS();
---
+++ shared/data-models/session.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+    this.sessionId;
+    this.zip;
+    this.state;
+    this.controlNum;
+};
