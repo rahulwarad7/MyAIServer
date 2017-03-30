64bc846a433863a6fb42d12e3cba910d6a90293c
rahulwarad123 <rahul.warad@gmail.com>
2017-03-30 14:39:26 +0530

    renters changes

--- shared/data-models/address.js
+++ shared/data-models/address.js
@@ -1,9 +1,13 @@
 function Address(){
-    this.addressLine1;
-    this.aptOrUnit;
-    this.city;
-    this.state;
-    this.zipCode;
+    this.addressLine1='';
+    this.aptOrUnit='';
+    this.city='';
+    this.state='';
+    this.zipCode='';
+    this.addressType='';
+    this.standardizationCodes=null;
+    this.invalidAddress=false;   
 }
+
 
 module.exports = Address;
